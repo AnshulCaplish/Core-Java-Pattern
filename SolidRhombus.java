033c5f7016edc059323e129d4832582573597cfa
Anshul Caplish <anshulsar@gmail.com>
2023-09-16 16:45:53 +0530

    Adding some more Pattern problems

---
+++ SolidRhombus.java
@@ -0,0 +1,29 @@
+package Pattern;
+
+/*  				SOLID RHOMBUS
+    *****
+   *****
+  *****
+ *****
+*****
+  
+ */
+
+public class SolidRhombus {
+
+	public static void main(String[] args) {
+		int n=5;
+		//int counter=1;
+		for(int i=1; i<=n; i++) {
+			//Spaces
+			for(int j=1; j<=n-i; j++) {
+					System.out.print(" ");
+			}
+			//Star
+			for(int j=1; j<=n;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
